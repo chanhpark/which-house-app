cb2d1f3d519e05d6a51b6393acfca33a31ea40ee
chanhpark <chanhyun.park1211@gmail.com>
2014-12-29 22:35:20 -0500

    added search, added routes

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -9,6 +9,11 @@ class HousesController < ApplicationController
   end
 
   def index
+
+  end
+
+  def show
+
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'houses#index'
   resources :houses do
     collection { post :import }
+      collection do
+        get 'search'
+      end
   end
-
 end
